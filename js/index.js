d281bfdff86ffd14892fad59a7414414d6642e57
Andrew <whitedrew538@gmail.com>
2021-11-01 22:40:12 +0500

    done

--- js/index.js
+++ js/index.js
@@ -2,16 +2,10 @@ window.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.menu'),
         menuItem = document.querySelectorAll('.menu_item'),
         hamburger = document.querySelector('.hamburger');
-    btn = document.querySelectorAll('button');
 
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('hamburger_active');
         menu.classList.toggle('menu_active');
     });
 
-    btn.forEach(element => {
-        element.addEventListener('click', () => {
-            alert("Sup man");
-        })
-    });
 })
