74fb24281ac37cf84af1ea6be421897e37aa8498
Andrew <whitedrew538@gmail.com>
2021-11-01 22:31:27 +0500

    some UX things

--- js/index.js
+++ js/index.js
@@ -2,10 +2,16 @@ window.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.menu'),
         menuItem = document.querySelectorAll('.menu_item'),
         hamburger = document.querySelector('.hamburger');
+    btn = document.querySelectorAll('button');
 
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('hamburger_active');
         menu.classList.toggle('menu_active');
     });
 
+    btn.forEach(element => {
+        element.addEventListener('click', () => {
+            alert("Sup man");
+        })
+    });
 })
