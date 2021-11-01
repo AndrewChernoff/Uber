cddea5bfd1889b5c56eb36c3973b3d0e5199e25d
Andrew <whitedrew538@gmail.com>
2021-11-01 18:45:22 +0500

    media for 320px

---
+++ js/index.js
@@ -0,0 +1,11 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+        menuItem = document.querySelectorAll('.menu_item'),
+        hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+    });
+
+})
